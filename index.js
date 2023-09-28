7d860aae29c6cf4ef727906d378d55dd6fc56cd0
Gokula <gokulakannanit@gmail.com>
2023-09-28 17:19:58 +0530

    initial commit

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/src/demo'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/src/demo/index.html'));
+    res.sendFile(path.join(__dirname+'/src/demo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
